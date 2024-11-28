a5a79c0170a083c52e9ebbbcafce96ef40514e0e
stefan.schirmeister <stefan.schirmeister@localiser.de>
2024-11-28 15:44:08 +0100

    lint html files

--- reenact/reenact/urls.py
+++ reenact/reenact/urls.py
@@ -6,34 +6,47 @@ from . import views
 app_name = "reenact"
 
 urlpatterns = [
-    # path('', views.home, name='home'),
-    path("", TemplateView.as_view(template_name="reenact/index.html")),
-    path("infos", TemplateView.as_view(template_name="reenact/infos.html")),
+    path("", TemplateView.as_view(template_name="reenact/index.html"), name="index"),
+    path(
+        "infos",
+        TemplateView.as_view(template_name="reenact/infos.html"),
+        name="infos",
+    ),
     path(
         "questionsDefault/regionToday",
         TemplateView.as_view(template_name="reenact/regionToday.html"),
+        name="region_today",
     ),
     path("pages/scenario", views.scenario, name="scenario"),
     path(
         "questionsDefault/scenariosOverview",
         TemplateView.as_view(template_name="reenact/scenariosOverview.html"),
+        name="scenarios_overview",
     ),
     path(
         "questionsDefault/createScenario",
         TemplateView.as_view(template_name="reenact/createScenario.html"),
+        name="create_scenario",
     ),
     path(
         "questionsDefault/scenarioDetail",
         TemplateView.as_view(template_name="reenact/scenarioDetail.html"),
+        name="scenario_detail",
     ),
     path(
         "questionsDefault/challenges",
         TemplateView.as_view(template_name="reenact/challenges.html"),
+        name="challenges",
     ),
     path(
         "pages/comparison",
         TemplateView.as_view(template_name="reenact/comparison.html"),
+        name="comparison",
     ),
-    path("source", TemplateView.as_view(template_name="reenact/source.html")),
-    path("help", TemplateView.as_view(template_name="reenact/help.html")),
+    path(
+        "source",
+        TemplateView.as_view(template_name="reenact/source.html"),
+        name="sources",
+    ),
+    path("help", TemplateView.as_view(template_name="reenact/help.html"), name="help"),
 ]
