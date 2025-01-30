33a2b72698ed6dbb0160524a5edc71ca31ed0d09
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-01-30 16:28:18 +0100

    Remove unnecessary urls and templates

--- config/urls.py
+++ config/urls.py
@@ -8,16 +8,9 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+    path("", include("reenact.reenact.urls")),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("", include("reenact.reenact.urls")),
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
