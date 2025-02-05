a831d42b8e0933770c874d328e96450b40aaf1cd
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-02-05 12:04:23 +0100

    Implement bar chart based on slider input

--- reenact/reenact/views.py
+++ reenact/reenact/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.views.generic.base import TemplateView
 
 from . import settings
@@ -11,3 +12,15 @@ class MainView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["capacities"] = CapacitiesForm(sliders=settings.SLIDERS)
         return context
+
+
+def chart(request, chart_name: str) -> JsonResponse:
+    """Return echart options as JSON."""
+    wind = request.GET.get("wind", 0.0)
+    pv = request.GET.get("pv", 0.0)
+    dummy_chart = {
+        "xAxis": {"type": "category", "data": ["Wind", "Photovoltaik"]},
+        "yAxis": {"type": "value"},
+        "series": [{"data": [wind, pv], "type": "bar"}],
+    }
+    return JsonResponse(dummy_chart)
