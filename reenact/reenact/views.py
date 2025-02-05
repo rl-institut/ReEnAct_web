80ede6d3dc3d896455fddf0640c2f630b8a48a69
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-02-05 14:19:34 +0100

    Minor change

--- reenact/reenact/views.py
+++ reenact/reenact/views.py
@@ -1,12 +1,9 @@
 from django.http import JsonResponse
 from django.views.generic.base import TemplateView
-from . import chart
-import json
 
 from . import settings
-from .forms import CapacitiesForm
 from .chart import generate_echarts_code
-
+from .forms import CapacitiesForm
 
 
 class MainView(TemplateView):
@@ -14,9 +11,6 @@ class MainView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        echarts_option = chart.generate_vertical_echarts_code(chart.production_data, chart.demand_data)
-        context["production_demand_chart"]= json.dumps(echarts_option, indent=2)
         context["capacities"] = CapacitiesForm(sliders=settings.SLIDERS)
         return context
 
@@ -47,9 +41,4 @@ def chart(request, chart_name: str) -> JsonResponse:
             item["value"] = float(pv)
 
     echarts_option = generate_echarts_code(production, demand)
-    dummy_chart = {
-        "xAxis": {"type": "category", "data": ["Wind", "Photovoltaik"]},
-        "yAxis": {"type": "value"},
-        "series": [{"data": [wind, pv], "type": "bar"}],
-    }
     return JsonResponse(echarts_option)
