775bc7d45dd2d5a165dfa95ff52188f14b914b80
stefan.schirmeister <stefan.schirmeister@localiser.de>
2025-02-12 15:02:25 +0100

    initial comparison pages

--- reenact/static/js/charts.js
+++ reenact/static/js/charts.js
@@ -33,3 +33,79 @@ function create_main_chart_on_startup() {
   const productionDemandChartOptions = JSON.parse(document.getElementById("productionDemandChart").textContent);
   createChart("mainChart", productionDemandChartOptions);
 }
+
+function reload_chart(name) {
+    // fetch and display scenario data in comparison charts
+    let select = document.getElementById('select-' + name);
+    let chartElement = document.getElementById('chart-' + name);
+    if (!chartElement)
+        return;
+
+    // get data from backend
+    let request = window.location.origin + '/chart/' + name + '_chart';
+    if (select) {
+        // selection: append query
+        request = request + '/?q=' + select.value;
+    }
+
+    fetch(request, {
+        method: 'GET',
+        mode: 'cors',
+        headers: new Headers({'Accept': 'application/json', 'Content-Type':'text/plain',}),
+        credentials: 'same-origin',
+    }).then(response => {response.json().then(data => {
+        let chart;
+        if (echarts.getInstanceByDom(chartElement)) {
+            chart = echarts.getInstanceByDom(chartElement);
+            chart.clear();
+        } else {
+            chart = echarts.init(chartElement, null, { renderer: "svg" });
+        }
+        let series = [];
+        for (let [k, v] of Object.entries(data.y_data)) {
+            series.push({
+                name: k,
+                data: v,
+                type: 'bar',
+            });
+        }
+        for (let [label, target] of Object.entries(data.target || {})) {
+            series.push({
+                name: label,
+                type: 'line',
+                markLine: {
+                    symbol: "none",  // endpoint symbol
+                    data: [{
+                        yAxis: target,
+                        lineStyle: {normal: {color: "#000"}}
+                    }],
+                }
+            });
+        }
+        const options = {
+            xAxis: {
+                type: 'category',
+                data: data.x_data,
+                axisLabel: {
+                    interval: 0,
+                    rotate: 30,
+                    //overflow: 'truncate', // or 'break' to continue in a new line
+                },
+            },
+            yAxis: {
+                type: 'value',
+                name: data.y_label,
+                nameLocation: "center",
+                nameRotate: 90,
+                nameTextStyle: {fontWeight: "bold"},
+            },
+            tooltip: {},
+            legend: {},
+            series: series,
+        };
+        chart.setOption(options);
+        chart.resize();
+    })}).catch (error => {
+        console.log(error);
+    });
+}
