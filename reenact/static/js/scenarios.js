bfa60cda4a4ab667ecfddab30165820c576f7bf8
sarah <sarah.sommer@rl-institut.de>
2025-02-17 08:36:10 +0100

    deleted comments

--- reenact/static/js/scenarios.js
+++ reenact/static/js/scenarios.js
@@ -1,13 +1,10 @@
 function updateScenario(title, description, button) {
-    // Alle Buttons deselektieren
     document.querySelectorAll('#scenarioTabs button').forEach(btn => {
         btn.classList.remove("selected");
     });
 
-    // Gewählten Button markieren
     button.classList.add("selected");
 
-    // Titel & Beschreibung aktualisieren
     document.getElementById("scenarioTitle").textContent = title;
     document.getElementById("scenarioDescription").textContent = description;
 }
