244d8b55d65381ac48bf72d82ec26208a3e8e1d7
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-03-19 15:10:53 +0100

    Add error message if scenario cannot be loaded

--- reenact/reenact/settings.py
+++ reenact/reenact/settings.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import json
 import pathlib
 from dataclasses import dataclass
+from json import JSONDecodeError
 
 APP_DIR = pathlib.Path(__file__).parent
 CONFIG_DIR = APP_DIR / "config"
@@ -37,6 +38,10 @@ for filename in sorted(SCENARIO_DIR.iterdir()):
     if filename.suffix == ".json":
         scenario_number = filename.name.split("_")[0]
         with filename.open("r", encoding="utf-8") as file:
-            data = json.load(file)
+            try:
+                data = json.load(file)
+            except JSONDecodeError as e:
+                error_msg = f"Could not load scenario {filename} due to JSON error."
+                raise RuntimeError(error_msg) from e
             data["number"] = int(scenario_number)
             SCENARIOS.append(data)
