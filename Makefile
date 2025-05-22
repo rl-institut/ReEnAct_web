096164de68da20f4444a40748ed7fe1700799eb1
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-05-22 10:35:44 +0200

    Update django_oemof to v1.0.0 and integrate emission tracking

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 
-.PHONY : update_vendor_assets, tailwind
+.PHONY : update_vendor_assets, tailwind, celery
+
+DJANGO_READ_DOT_ENV_FILE=True
+export
+
+celery:
+	redis-server --port 6380 & celery -A config.celery_app worker -l INFO
 
 tailwind:
 	npx tailwindcss -i reenact/static/css/tailwind_input.css -o reenact/static/css/tailwind.css
---
+++ scripts/django_oemof_standalone.py
@@ -0,0 +1,19 @@
+import logging
+import time
+
+from django_oemof.standalone import init_django
+
+init_django(installed_apps=["reenact.reenact"])
+from django_oemof import simulation  # noqa: E402
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+PARAMETERS = {"test": "emissions"}
+
+start = time.time()
+simulation_id = simulation.simulate_scenario(scenario="es5", parameters=PARAMETERS)
+lg_msg = f"Simulation Time: {time.time() - start}"
+logger.info(lg_msg)
+lg_msg = f"Simulation ID: {simulation_id}"
+logger.info(lg_msg)
