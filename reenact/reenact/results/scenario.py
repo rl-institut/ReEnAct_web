7a59d5411456e016501f71e24809b495771ae693
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-07-03 14:52:04 +0200

    Init chart and potentials from my-plan user input if given

--- reenact/reenact/results/scenario.py
+++ reenact/reenact/results/scenario.py
@@ -3,11 +3,30 @@
 from __future__ import annotations
 
 from django_oemof.models import Simulation
+from reenact.reenact import hooks
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+
+
+def get_simulation_results_from_request(
+    scenario: str,
+    request: HttpRequest,
+) -> int | None:
+    """
+    Return simulation ID for a given scenario and user input from the request.
+
+    To get results, setup hooks are applied.
+    """
+    parameters = hooks.set_up_volatiles("", request.GET, None)
+    return get_simulation_results(scenario, parameters)
 
 
 def get_simulation_results(scenario: str, parameters: dict) -> int | None:
     """
-    Return simulation ID for given scenario and parameters.
+    Return simulation ID for a given scenario and parameters.
 
     Return None if no simulation ID is found.
     """
--- reenact/reenact/views.py
+++ reenact/reenact/views.py
@@ -35,8 +35,29 @@ class MainView(TemplateView):
             for slider_config in settings.SLIDERS
         }
         context["capacities"] = CapacitiesForm(self.request.GET)
-        if not context["capacities"].is_valid():
+        if context["capacities"].is_valid():
+            # Load potentials and capacity chart from user input
+            my_plan_potentials = self.request.GET
+            my_plan_capacities = capacities.get_chart_data_from_user_input(
+                self.request.GET,
+            )
+            # Check if simulation exists for user input
+            simulation_id = scenario.get_simulation_results_from_request(
+                SCENARIOS[0].get("oemof_scenario", ""),
+                self.request,
+            )
+        else:
+            # User input is not valid, thus default my-plan scenario gets loaded
             context["capacities"] = CapacitiesForm()
+            my_plan_potentials = {
+                name: data["initial"] for name, data in settings.SLIDER_DATA.items()
+            }
+            my_plan_capacities = capacities.get_chart_data_from_user_input({})
+            simulation_id = scenario.get_simulation_results(
+                SCENARIOS[0].get("oemof_scenario", ""),
+                {},
+            )
+
         context["scenarios"] = settings.SCENARIOS
 
         results = boxes.get_result_boxes_from_scenario_data(SCENARIOS[0])
@@ -47,27 +68,20 @@ class MainView(TemplateView):
                 settings.SCENARIOS[0],
             ),
         )
-        my_plan_potentials = {
-            name: data["initial"] for name, data in settings.SLIDER_DATA.items()
-        }
-        context["potentials_my_plan"] = potentials.add_wetland_potential(
-            potentials.calculate_potentials_from_request(
-                my_plan_potentials,
-            ),
-        )
 
         # Get production and demand for status quo and my plan
-        simulation_id = scenario.get_simulation_results(
-            SCENARIOS[0].get("oemof_scenario", ""),
-            {},
-        )
         if simulation_id is None:
             context.update(capacities.get_chart_data_from_scenario(0))
         else:
             context.update(
                 capacities.get_chart_data_from_oemof_simulation(simulation_id),
             )
-        my_plan_capacities = capacities.get_chart_data_from_user_input({})
+
+        context["potentials_my_plan"] = potentials.add_wetland_potential(
+            potentials.calculate_potentials_from_request(
+                my_plan_potentials,
+            ),
+        )
         context["production_my_plan"] = my_plan_capacities["production"]
         context["demand_my_plan"] = my_plan_capacities["demand"]
         context["slider_dependencies"] = settings.SLIDER_DEPENDENCIES
