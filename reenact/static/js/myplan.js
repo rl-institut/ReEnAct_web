9855f5d12d66be1bdd95bd5381fa555a2a01a761
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-07-03 12:38:18 +0200

    Change URL when changing capacities; Copy URL to clipboard

--- reenact/static/js/myplan.js
+++ reenact/static/js/myplan.js
@@ -18,6 +18,7 @@ const msgSimulation = "Berechnung gestartet...";
 function capacitiesChanged() {
   const capacityForm = document.getElementById("capacityForm");
   const formData = new FormData(capacityForm);
+  updateURL(formData);
   update_chart("myplan-chart", formData);
   update_potentials();
   capacitiesChangedSimulation();
@@ -83,3 +84,18 @@ function showResults(simulationId) {
   document.querySelectorAll(".myplanUpdateMsg").forEach(element => element.classList.add("hidden"));
   // update_chart("myplan-chart", {simulationId: simulationId});  // in charts.js
 }
+
+function updateURL(formData) {
+  const searchParams = new URLSearchParams(formData);
+  const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+  window.history.pushState({path: newUrl}, '', newUrl);
+}
+
+function copyURLToClipboard() {
+    navigator.clipboard.writeText(window.location.href);
+    const notification = document.getElementById('copyNotification');
+    notification.classList.remove('hidden');
+    setTimeout(() => {
+      notification.classList.add('hidden');
+    }, 2000);
+  }
