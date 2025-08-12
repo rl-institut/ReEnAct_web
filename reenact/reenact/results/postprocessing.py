697d6a7a78583fca5ccd5dd50212213c960f8fc0
Matthias Zander <matthias.zander@posteo.de>
2025-08-12 09:44:18 +0200

    two small changes to default values

--- reenact/reenact/results/postprocessing.py
+++ reenact/reenact/results/postprocessing.py
@@ -26,7 +26,7 @@ def co2_ems(inp, outp):
     cost = 0.0
 
     production_goal = 401100  # in MWh # TODO: dynamisieren
-    marsh_dry = 5  # in ha # TODO: dynamisieren
+    marsh_dry = 0  # in ha # TODO: dynamisieren: mit User-Input für Widervernaessung verknuepfen
     mt_co2_ems = 20  # in t_CO2/ha # TODO: dynamisieren
     produced = prod(inp, outp)
     co2_index = 0.20088  # in t_CO2/MWh # TODO: dynamisieren
