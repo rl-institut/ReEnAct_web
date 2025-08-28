c749005b09df390935d8f3607bd1965858154d39
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-08-28 14:32:07 +0200

    Remove model hooks

--- reenact/reenact/apps.py
+++ reenact/reenact/apps.py
@@ -20,28 +20,3 @@ class ReenactConfig(AppConfig):
                 function=reenact_hooks.set_up_oemof_components_from_user_input,
             ),
         )
-
-        hooks.register_hook(
-            hook_type=hooks.HookType.MODEL,
-            hook=hooks.Hook(hooks.ALL_SCENARIOS, reenact_hooks.model_co2_tracking),
-        )
-        hooks.register_hook(
-            hook_type=hooks.HookType.MODEL,
-            hook=hooks.Hook(hooks.ALL_SCENARIOS, reenact_hooks.model_co2_limit),
-        )
-        hooks.register_hook(
-            hook_type=hooks.HookType.MODEL,
-            hook=hooks.Hook(hooks.ALL_SCENARIOS, reenact_hooks.model_co2_cost),
-        )
-        hooks.register_hook(
-            hook_type=hooks.HookType.MODEL,
-            hook=hooks.Hook(hooks.ALL_SCENARIOS, reenact_hooks.model_prod_goal),
-        )
-
-        hooks.register_hook(
-            hooks.HookType.POSTPROCESSING,
-            hooks.Hook(
-                scenario=hooks.ALL_SCENARIOS,
-                function=reenact_hooks.store_emission,
-            ),
-        )
